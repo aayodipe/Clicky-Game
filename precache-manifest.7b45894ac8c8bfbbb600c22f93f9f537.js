1351d70667bc50e93a6d972a8b510a598342300c
Adeyemi Ayodipe <ayodipeadeyemi@gmail.com>
2019-04-16 00:14:55 -0500

    Updates

---
+++ precache-manifest.7b45894ac8c8bfbbb600c22f93f9f537.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1ba6c11ff0b539dad6c7",
+    "url": "/aayodipe/Clicky-Game/static/js/runtime~main.1e15d36e.js"
+  },
+  {
+    "revision": "c6ed8f272b538446688d",
+    "url": "/aayodipe/Clicky-Game/static/js/main.540ad737.chunk.js"
+  },
+  {
+    "revision": "f85467252c3594ca745c",
+    "url": "/aayodipe/Clicky-Game/static/js/2.abe55e13.chunk.js"
+  },
+  {
+    "revision": "c6ed8f272b538446688d",
+    "url": "/aayodipe/Clicky-Game/static/css/main.16dadd97.chunk.css"
+  },
+  {
+    "revision": "f85467252c3594ca745c",
+    "url": "/aayodipe/Clicky-Game/static/css/2.af9d60f2.chunk.css"
+  },
+  {
+    "revision": "588cc85aab6746c6bc46573950a3d7a6",
+    "url": "/aayodipe/Clicky-Game/index.html"
+  }
+];
