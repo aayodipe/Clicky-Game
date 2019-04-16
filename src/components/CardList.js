2d3c17c25397cbf6de28b6268cd3328d3e6fb16c
Adeyemi Ayodipe <ayodipeadeyemi@gmail.com>
2019-04-15 23:47:41 -0500

    Added Top Score functionality

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -4,62 +4,73 @@ import Card from './Card'
 import robots from '../robots';
 import Header from './Header'
 import css from '../index'
+import Jumbotron from './Jumbotron'
 
 // create a function that that render the list of cards with images using map function in react
-
-const arr = []
 class Cardlist extends Component{
      state = {
           instruction: 'Click any Robot to get started',
           robots:robots,
           score:0,
           topScore :0,
-          RobotAlreadyClicked : []
+          RobotAlreadyClicked : [],
+     }
+    
+     handleRobotClick = (event)=>{
+           event.preventDefault()
+          // Get the name of the Robot 
+           const itemClicked = event.target.alt; 
+         //Sort the Robot
+     const sortedRobots = robots.sort(function(){ return Math.random()-0.5}) 
+     // Check if the robot is already click
+
+      let isRobotAlreadyClicked = this.state.RobotAlreadyClicked.includes(itemClicked)
+      console.log(isRobotAlreadyClicked)
+   if(!isRobotAlreadyClicked) {
+     const updatedTopScore = this.state.score >= this.state.topScore
+          ? this.state.score + 1
+          : this.state.topScore; 
+
+     this.setState({
+          RobotAlreadyClicked: [...this.state.RobotAlreadyClicked, itemClicked],
+          instruction: 'Hurraaay! Thanks for selecting me',     
+          robots:sortedRobots,
+          score:this.state.score + 1,
+          topScore: updatedTopScore,    
+     })
+    console.log(this.state.score, this.state.updatedScore)
+   } else{
+          this.setState({
+               RobotAlreadyClicked:[],
+               instruction:  'Oh no! I have been selected',     
+               robots:sortedRobots,
+               score:0,     
+          }) 
+          console.log(this.state.RobotAlreadyClicked)
      }
 
-
-
-//Sort and increment on click of an image
-     handleRobotClick = (event)=>{
-        const itemClicked = event.target.alt;  
-        const RobotAlreadySelected = arr.includes(itemClicked)
-        console.log(arr)
-        const sortedRobots = robots.sort(function(){ return Math.random()-0.5})  
-        console.log(RobotAlreadySelected)
-        if(!RobotAlreadySelected){
-           
-            this.setState({instruction: 'Hurraaay! Thanks for selecting me',     
-            robots: sortedRobots,
-            score : this.state.score + 1,
-            topScore : this.state.topScore + 1
-         })
-        }else{
-           this.setState({
-               instruction: 'Oh no! I have been selected',
-               robots: sortedRobots,
-               score :0
-        })
-        
-        }
-        arr.push(itemClicked)         
-        this.setState({RobotAlreadyClicked:arr})}
-   
-     //
+     if(this.state.score === this.state.updatedScore){
+          console.log(this.state.updatedScore)
+       this.setState({topScore:this.state.updatedScore+1})
+     }
+}
      render(){
- return (
-     <div>
-           <Header  instruction = {this.state.instruction} score = {this.state.score}  topScore = {this.state.topScore}/>
-     
-      {     
-           this.state.robots.map((robot, i)=>(
-                
-               <Card 
-               name = {robot.name } 
-               id = {robot.id} 
-               handleRobotClick ={this.handleRobotClick} 
-               key = {i}/> 
-           )) 
-      }
-    </div>
- )}}
-export default Cardlist
+          return (
+              <div>
+                    <Header  instruction = {this.state.instruction} score = {this.state.score}  topScore = {this.state.topScore}/>
+                   <Jumbotron />
+               {     
+                    this.state.robots.map((robot, i)=>(
+                         
+                        <Card 
+                        name = {robot.name } 
+                        id = {robot.id} 
+                        handleRobotClick ={this.handleRobotClick} 
+                        key = {i}/> 
+                    )) 
+               }
+         
+             </div>
+          )}}
+
+          export default Cardlist
--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -4,12 +4,9 @@ import React from 'react'
 const Jumbotron = ()=>{
      return (
           <div className="jumbotron">
-          <h1 className="display-4">Hello, world!</h1>
-          <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-          <hr className="my-4" />
-          <p>It uses utility classNamees for typography and spacing to space content out within the larger container.</p>
-          <a className="btn btn-primary btn-lg" href="/" role="button">Learn more</a>
-        </div>
+          <h1 className="display-4 text-center font-weight-bold">Clicky Game!</h1>
+          <p className = "h3 text-center ">Click on a Robot to earn points, but don't click on any more than once! </p>
+          </div>
      )
 }
 
